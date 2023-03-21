e463890c2b2d3aa4909dc47739b640bbe76395e7
Adrian Novegil <adrian.novegil@gmail.com>
2023-03-21 17:57:05 +0100

    Remove the registry and history modules

--- all-in-one-runner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
+++ all-in-one-runner/src/main/java/eu/neclab/ngsildbroker/runner/Runner.java
@@ -12,10 +12,14 @@ import eu.neclab.ngsildbroker.subscriptionmanager.SubscriptionHandler;
 @SpringBootApplication
 public class Runner {
 
-	public static void main(String[] args) throws Exception {
-		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
-		SpringApplication.run(new Class[] { RegistryHandler.class, HistoryHandler.class, QueryHandler.class,
-				RegistrySubscriptionHandler.class, EntityHandler.class, SubscriptionHandler.class }, args);
-	}
-
+    public static void main(String[] args) throws Exception {
+        System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
+        SpringApplication.run(new Class[]{
+            RegistryHandler.class,
+            HistoryHandler.class,
+            QueryHandler.class,
+            RegistrySubscriptionHandler.class,
+            EntityHandler.class,
+            SubscriptionHandler.class}, args);
+    }
 }
